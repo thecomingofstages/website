00275cd9b9365cebd9081fe20bffdb6d47f77ead
Sakditach Thanasodsai <lemascserviceth@hotmail.com>
2024-04-06 20:21:55 +0000

    setup theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,26 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        stage: {
+          DEFAULT: "#E30A04",
+          50: "#FFF2F1",
+          100: "#FEDEDD",
+          200: "#FEB7B5",
+          300: "#FD908D",
+          400: "#FC6965",
+          500: "#FC423D",
+          600: "#FB1B15",
+          700: "#E30A04",
+          800: "#AC0803",
+          900: "#750502",
+          950: "#590402",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-prompt)", "sans-serif"],
+        serif: ["var(--font-stix-two-text)", "serif"],
+      },
     },
   },
   plugins: [],
