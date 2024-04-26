ace02a5dbbdbdbccda4e61e4fb0abaf5c894b2d8
Lemasc Service <lemascserviceth@hotmail.com>
2024-04-26 19:53:58 +0700

    cloudflare deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,8 @@ const bodyFont = Prompt({
   weight: ["400", "500", "700"],
 });
 
+export const runtime = "edge";
+
 export const metadata: Metadata = {
   title: {
     template: "%s | The Coming of Stages",
