cd3c0448ef9f2f8a4c119f10fba4958b7173bbbd
AZDEV2006 <120896393+AZDEV2006@users.noreply.github.com>
2024-05-02 00:39:25 +0700

    Update Gsap 3W

--- app/home/sections/Know.tsx
+++ app/home/sections/Know.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
 
 import {
   AskIcon,
@@ -9,7 +9,10 @@ import {
   PeopleIcon,
 } from "../graphics/know";
 import { Section } from "./base";
-
+import { useGSAP } from '@gsap/react';
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScrollToPlugin from "gsap/ScrollToPlugin";
 const DetailSection = ({
   title,
   children,
@@ -20,8 +23,8 @@ const DetailSection = ({
   Icon: React.FC<{ className: string }>;
 }) => {
   return (
-    <div className="flex w-full flex-row py-4 items-center justify-center gap-8">
-      <div className="flex flex-col gap-4 flex-grow max-w-xl">
+    <div className="flex w-screen flex-col md:flex-row py-4 items-center justify-cente gap-8">
+      <div className="flex flex-col gap-4 flex-grow max-w-xl w-full md:w-[500px]">
         <h3 className="text-4xl font-bold font-head inline-flex items-center gap-2">
           <Icon className="w-20 h-20 text-white" />
           {title}
@@ -44,56 +47,109 @@ const Desc = ({ className, ...props }: React.ComponentPropsWithoutRef<"p">) => {
   );
 };
 
+interface StaticData_i {
+  title: string,
+  Icon: React.FC<{ className: string }>,
+  description: Array<String>
+}
+
 export const GettingToKnowUsSection = () => {
-  const rootRef = useRef<HTMLElement | null>(null);
+  // Ref for use in gsap!
+  const wrapper = useRef<HTMLElement | null>(null);
+  const sectionRef = useRef<HTMLElement>(null) as MutableRefObject<HTMLDivElement>;
+  gsap.registerPlugin(ScrollTrigger);
+
+  const StaticData: StaticData_i[] = [
+    {
+      title: "Who?",
+      Icon: PeopleIcon,
+      description: [`
+      พวกเราคือกลุ่มเยาวชนไทยที่เล็งเห็นถึงความเป็นไปได้ในการต่อยอด
+      เเละพัฒนาความสามารถของเยาวชนรุ่นใหม่ โดยเกิดจากการร่วมมือของนักเรียน
+      นักศึกษา จากหลากหลายสถาบัน
+      แต่มีเป้าหมายเดียวกันคือการพัฒนาศักยภาพการทำงานอย่างไร้ขีดจํากัด
+      เพื่อขับเคลื่อนวงการละครเวทีไทยเเละคงไว้ซึ่งคุณค่าสืบต่อไป
+      `]
+    },
+    {
+      title: "What?",
+      Icon: BusinessIcon,
+      description: [`
+      พวกเราคือกลุ่มเยาวชนไทยที่เล็งเห็นถึงความเป็นไปได้ในการต่อยอด
+      เเละพัฒนาความสามารถของเยาวชนรุ่นใหม่ โดยเกิดจากการร่วมมือของนักเรียน
+      นักศึกษา จากหลากหลายสถาบัน
+      แต่มีเป้าหมายเดียวกันคือการพัฒนาศักยภาพการทำงานอย่างไร้ขีดจํากัด
+      เพื่อขับเคลื่อนวงการละครเวทีไทยเเละคงไว้ซึ่งคุณค่าสืบต่อไป
+      `, `พวกเรามีความตั้งใจที่จะส่งเสริมศักยภาพของเด็กไทย
+      และขับเคลื่อนวงการละครเวทีไทยให้ก้าวหน้ายิ่งขึ้น
+      ผ่านการสร้างโปรเจคเพื่อเปิดโอกาสให้เด็กไทยได้สะสมประสบการณ์การทำงานในสถานที่จริง
+      อีกทั้งยังอยากเป็นแรงบันดาลใจให้กับเด็กทุกคนที่มีความฝัน
+      ให้กล้าที่จะลุกขึ้นมาทำสิ่งดีๆเพื่อตนเองเเละสังคม`]
+    },
+    {
+      title: "Why?",
+      Icon: BusinessIcon,
+      description: [`
+      ในปัจจุบันละครเวทีในประเทศไทยยังไม่ได้เป็นที่นิยม
+      เมื่อเทียบกับละครเวทีในต่างประเทศ เเต่ทว่า
+      ก็ยังมีเด็กไทยจำนวนมากที่มีความชอบและความสนใจในด้านนี้
+      เเต่เพียงขาดโอกาสในการต่อยอดและพัฒนา
+      `, `พวกเราจึงมีความตั้งใจอย่างมากที่จะสร้างสถานที่
+      ที่สามารถลงมือทําได้จริง รวมกับความเชื่อที่ว่า
+      เด็กไทยนั้นมีความสามารถมากพอที่จะประสบความสำเร็จได้
+      จากเเรงสนับสนุนและการร่วมแรงร่วมใจกัน`]
+    },
+
+  ]
+
+  useEffect(() => {
+    const pin = gsap.fromTo(sectionRef.current,
+      {
+        translateX: 0,
+      },
+      {
+        translateX: "-1800px",
+        ease: "none",
+        duration: 1,
+
+        scrollTrigger: {
+          trigger: wrapper.current,
+          start: "center center",
+          end: () => "+=300",
+          scrub: 0.6,
+          pin: true,
+          // markers: true
+        }
+      }
+    )
+
+    return () => {
+      pin.kill();
+    };
+  }, [])
+
 
   return (
     <div id="know-us">
       <Section
-        ref={rootRef}
+        ref={wrapper}
         className="flex flex-col flex-grow gap-6 justify-center relative"
       >
-        <div className="flex flex-col gap-4 pt-36 pb-8 sticky top-0 bg-black">
+        <div className="flex flex-col gap-4 pt-36 pb-8 top-0 bg-black">
           <h2 className="text-6xl font-bold font-head">Getting to know us</h2>
           <p>Short description about what is going on earth.</p>
         </div>
-        <DetailSection title={"Who?"} Icon={PeopleIcon}>
-          <Desc>
-            พวกเราคือกลุ่มเยาวชนไทยที่เล็งเห็นถึงความเป็นไปได้ในการต่อยอด
-            เเละพัฒนาความสามารถของเยาวชนรุ่นใหม่ โดยเกิดจากการร่วมมือของนักเรียน
-            นักศึกษา จากหลากหลายสถาบัน
-            แต่มีเป้าหมายเดียวกันคือการพัฒนาศักยภาพการทำงานอย่างไร้ขีดจํากัด
-            เพื่อขับเคลื่อนวงการละครเวทีไทยเเละคงไว้ซึ่งคุณค่าสืบต่อไป
-          </Desc>
-        </DetailSection>
-        <DetailSection title={"What?"} Icon={BusinessIcon}>
-          <Desc>
-            พวกเรามีความตั้งใจที่จะส่งเสริมศักยภาพของเด็กไทย
-            และขับเคลื่อนวงการละครเวทีไทยให้ก้าวหน้ายิ่งขึ้น
-            ผ่านการสร้างโปรเจคเพื่อเปิดโอกาสให้เด็กไทยได้สะสมประสบการณ์การทำงานในสถานที่จริง
-            อีกทั้งยังอยากเป็นแรงบันดาลใจให้กับเด็กทุกคนที่มีความฝัน
-            ให้กล้าที่จะลุกขึ้นมาทำสิ่งดีๆเพื่อตนเองเเละสังคม
-          </Desc>
-          <Desc>
-            องค์กรของเรามีเป้าหมายที่จะทำการจัดการแสดงละครเวทีในทุกปี
-            โดยในแต่ละปี จะมีการเปิดรับสมัครนักเรียน-นักศึกษา ในช่วงอายุ 16-20
-            เพื่อเปิดโอกาสในการพัฒนาดังกล่าว
-          </Desc>
-        </DetailSection>
-        <DetailSection title={"Why?"} Icon={AskIcon}>
-          <Desc>
-            ในปัจจุบันละครเวทีในประเทศไทยยังไม่ได้เป็นที่นิยม
-            เมื่อเทียบกับละครเวทีในต่างประเทศ เเต่ทว่า
-            ก็ยังมีเด็กไทยจำนวนมากที่มีความชอบและความสนใจในด้านนี้
-            เเต่เพียงขาดโอกาสในการต่อยอดและพัฒนา
-          </Desc>
-          <Desc>
-            พวกเราจึงมีความตั้งใจอย่างมากที่จะสร้างสถานที่
-            ที่สามารถลงมือทําได้จริง รวมกับความเชื่อที่ว่า
-            เด็กไทยนั้นมีความสามารถมากพอที่จะประสบความสำเร็จได้
-            จากเเรงสนับสนุนและการร่วมแรงร่วมใจกัน
-          </Desc>
-        </DetailSection>
+        <div ref={sectionRef} className="flex gap-4">
+          {
+            StaticData.map((data: StaticData_i, idx: number) => (
+              <DetailSection key={idx} title={data.title} Icon={data.Icon}>
+                {data.description.map((desc, index) => (
+                  <Desc key={index}>{desc}</Desc>
+                ))}
+              </DetailSection>
+            ))
+          }
+        </div>
       </Section>
     </div>
   );
