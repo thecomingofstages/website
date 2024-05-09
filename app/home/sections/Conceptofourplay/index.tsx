4174da8290a23ddf5442a5ac414ce4d3b014166a
AZDEV2006 <120896393+AZDEV2006@users.noreply.github.com>
2024-05-09 11:32:49 +0700

    Update index.tsx

--- app/home/sections/Conceptofourplay/index.tsx
+++ app/home/sections/Conceptofourplay/index.tsx
@@ -12,13 +12,13 @@ const Conceptofourplay = () => {
                 <Section className={{ wrapper: "relative", content: "px-0" }}>
                     <div className="absolute w-full h-screen top-0 left-0 bg-red-50">
                         <div id="background" className='w-full h-full object-contain'>
-                            <Image src={background} width={0} height={0} className='object-cover min-h-full' />
+                            <Image alt="background" src={background} width={0} height={0} className='object-cover min-h-full' />
                         </div>
                         <div id="Details" className='absolute top-0 h-full'>
-                            <Image src={Text} width={0} height={0} className='object-cover min-h-[80%]' />
+                            <Image alt="Details" src={Text} width={0} height={0} className='object-cover min-h-[80%]' />
                         </div>
                         <div id="gimmig" className='absolute min-h-full h-full top-0'>
-                            <Image src={Gim} width={0} height={0} className='object-cover min-h-full' />
+                            <Image alt="gimmig" src={Gim} width={0} height={0} className='object-cover min-h-full' />
                         </div>
                     </div>
                 </Section>
