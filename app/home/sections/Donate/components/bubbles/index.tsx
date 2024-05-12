11cb50a8900a277bacc26cd8202c69acba7892bb
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2024-05-12 22:59:34 +0800

    disclosure box right

--- app/home/sections/Donate/components/bubbles/index.tsx
+++ app/home/sections/Donate/components/bubbles/index.tsx
@@ -18,25 +18,27 @@ export const Bubble = ({ bubbleFront, content, title }: BubbleData) => {
   //   </Disclosure>
   // );
   return (
-    <Disclosure as="div" className="relative w-full max-w-md">
-      <DisclosureButton className="w-full pb-2">
+    <Disclosure as="div" className="flex flex-row relative ">
+      <DisclosureButton className="w-full pb-2 z-10">
         <Image src={bubbleFront} alt={title} width={200} height={200} />
       </DisclosureButton>
 
-      <div className="w-full h-full overflow-hidden ">
+      <div className="w-full h-full overflow-hidden">
         <Transition
           enter="duration-200 ease-out"
           enterFrom="opacity-0 -translate-x-6"
           enterTo="opacity-100 translate-x-0"
           leave="duration-300 ease-out"
           leaveFrom="opacity-100 translate-x-0"
-          leaveTo="opacity-0 -translate-x-6"
+          leaveTo="opacity-0 -translate-x-2"
         >
-          <DisclosurePanel className="transition w-full">
-            <div className="w-70 h-70 border-white border rounded-r-3xl ">
-              <p className="p-10">{content}</p>
+          <DisclosurePanel className="absolute transition left-12 z-0 mt-0">
+            <div className=" border-white border rounded-r-3xl h-24 w-80">
+              <div id="content" className="ml-10">
+                <p className="p-5 text-xs leading-normal">{content}</p>
+              </div>
+              
             </div>
-            
           </DisclosurePanel>
         </Transition>
       </div>
