6953837eea9793041d3c4e3d07c36a7137419a67
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2024-05-22 18:39:13 +0700

    slip image

--- app/api/donate/slipImage/route.tsx
+++ app/api/donate/slipImage/route.tsx
@@ -1,48 +1,72 @@
 /* eslint-disable jsx-a11y/alt-text */
 
 /* eslint-disable @next/next/no-img-element */
-import { ImageResponse } from "next/og";
-import { ImageResponseOptions, NextRequest } from "next/server";
+import { ImageResponse, } from "next/og";
+import { ImageResponseOptions, NextRequest, } from "next/server";
 
-import { ReceiptColor, ReceiptTheme } from "./types";
-import { ErrorImage, FontOptions, getFonts, getLocalAsset } from "./utils";
+import { ReceiptColor, ReceiptTheme,  } from "./types";
+import { FontOptions, getFonts, getLocalAsset,  } from "./utils";
+import dayjs from "dayjs";
+import "dayjs/locale/th";
 
 export const runtime = "edge";
 
-// todo: implement themes here
 const themes: ReceiptTheme = {
   green: {
     bgImageUrl: new URL(`./graphics/receipt-green.png`, import.meta.url),
-    orderColor: "white",
+    orderColor: "#ff6700",
+    nameColor: "#ff6700",
+    dateTimeColor: "#ff6700",
   },
   purple: {
     bgImageUrl: new URL(`./graphics/receipt-purple.png`, import.meta.url),
+    orderColor: "#ffe500",
+    nameColor: "#ffe500",
+    dateTimeColor: "#ffe500",
   },
   red: {
     bgImageUrl: new URL(`./graphics/receipt-red.png`, import.meta.url),
+    orderColor: "#000d03",
+    nameColor: "#000d03",
+    dateTimeColor: "#000d03",
   },
   white: {
     bgImageUrl: new URL(`./graphics/receipt-white.png`, import.meta.url),
+    orderColor: "#000d03",
+    nameColor: "#000d03",
+    dateTimeColor: "#000d03",
   },
 };
 
 const fonts: FontOptions[] = [
   {
-    name: "Prompt",
+    name: "Prompt-regular",
     url: new URL(`./fonts/Prompt-Regular.ttf`, import.meta.url),
     weight: 400,
   },
   {
-    name: "Prompt",
+    name: "Prompt-medium",
     url: new URL(`./fonts/Prompt-Medium.ttf`, import.meta.url),
     weight: 500,
   },
+  // {
+  //   name: "Source Serif Italic",
+  //   url: new URL(`./fonts/SourceSerif-Italic.ttf`, import.meta.url),
+  //   weight: 400, 
+  // },
+
 ];
 
 export const GET = async (request: NextRequest) => {
   const params = request.nextUrl.searchParams;
   const color = params.get("color") as ReceiptColor;
   const theme = themes[color];
+  const amount = 1000;
+  const amountString = amount.toLocaleString();
+  const name = "สมชาย ใจดี";
+  const date = new Date(2024, 5, 1, 22, 50, 0, 0);
+  const day = dayjs(date).format('MM/DD/YY')
+  const time = dayjs(date).format('HH:mm')
   try {
     if (!theme) {
       throw `Invalid color and theme: ${color}`;
@@ -55,17 +79,48 @@ export const GET = async (request: NextRequest) => {
     };
     return new ImageResponse(
       (
-        <div tw="relative w-full h-full flex flex-col font-[Prompt]">
+        <div tw="relative w-full h-full flex flex-col">
           <img tw="absolute inset-0 w-full h-full" src={bgImage} />
-          {/** add content here! */}
-          <div tw="relative inset-0 left-[50px] flex flex-col w-full text-black">
+          <div tw="relative inset-0 flex flex-col w-full">
             <span
-              tw="absolute top-[600px] text-5xl"
+              tw="absolute top-[123px] right-[35px] text-2xl"
               style={{
                 color: theme.orderColor,
               }}
             >
-              Example Text
+              00001
+            </span>
+            <span 
+              tw="absolute top-[710px] left-[50px] text-5xl "
+              style={{
+                color: theme.nameColor,
+              }}
+              >
+              {name}
+            </span>
+            <span 
+              tw="absolute top-[808px] right-[50px] text-4xl "
+              style={{
+                color: theme.dateTimeColor,
+              }}
+              >
+              {amountString}฿
+            </span>
+            <span 
+              tw="absolute top-[868px] right-[50px] text-4xl "
+              style={{
+                color: theme.dateTimeColor,
+              }}
+              >
+              {day}
+            </span>
+            <span 
+              tw="absolute top-[928px] right-[50px] text-4xl "
+              style={{
+                color: theme.dateTimeColor,
+              }}
+              >
+              {time}
             </span>
           </div>
         </div>
@@ -73,7 +128,7 @@ export const GET = async (request: NextRequest) => {
       imageOptions
     );
   } catch (err) {
-    //console.error(err);
-    return ErrorImage(err as never, imageOptions);
+    // console.error(err);
+    // return ErrorImage(err as never, imageOptions);
   }
 };
